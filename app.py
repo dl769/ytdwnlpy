fb6a2c16dd91baffff5deaceef9bceecd1bd021e
dl769 <qwertyx7xx@gmail.com>
2020-08-01 14:09:22 +0200

    first

--- app.py
+++ app.py
@@ -9,9 +9,20 @@ CORS(app)
 @app.route('/', methods=['POST'])
 def dwn():
     url = request.form['url']
+    ext = request.form['ext']
+    qal = request.form['qal']
     name = url[32:43]
 
-    system('youtube-dl -f 251 -o "'+name+'1" '+url)
+    if (ext == "mp3"):
+            system('youtube-dl --extract-audio --add-metadata --audio-format "mp3" --embed-thumbnail -o "'+name+'1" '+url)
+        elif (ext == "mp4"):
+            if(qal == "720"):
+                system('youtube-dl --add-metadata -f "best[height<=720]" --recode-video mp4 -o "'+name+'1" '+url)
+            elif(qal == "1080"):
+                system('youtube-dl --add-metadata -f "best[height<=1080]" --recode-video mp4 -o "'+name+'1" '+url)
+            elif(qal == "9999"):
+                system('youtube-dl --add-metadata -f "best" --recode-video mp4 -o "'+name+'1" '+url)
+
     ret = send_file(name+'1', as_attachment=True)
     return ret
 
