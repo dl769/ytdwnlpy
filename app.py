d9f04ba906e2b7c6ebb0ac4f13be1a95dabb14be
dl769 <qwertyx7xx@gmail.com>
2020-08-01 14:16:20 +0200

    first

--- app.py
+++ app.py
@@ -12,17 +12,22 @@ def dwn():
     ext = request.form['ext']
     qal = request.form['qal']
     name = url[32:43]
+    error = 0 
 
     if (ext == "mp3"):
             system('youtube-dl --extract-audio --add-metadata --audio-format "mp3" --embed-thumbnail -o "'+name+'1" '+url)
-        elif (ext == "mp4"):
-            if(qal == "720"):
-                system('youtube-dl --add-metadata -f "best[height<=720]" --recode-video mp4 -o "'+name+'1" '+url)
-            elif(qal == "1080"):
-                system('youtube-dl --add-metadata -f "best[height<=1080]" --recode-video mp4 -o "'+name+'1" '+url)
-            elif(qal == "9999"):
-                system('youtube-dl --add-metadata -f "best" --recode-video mp4 -o "'+name+'1" '+url)
-
+    elif (ext == "mp4"):
+        if(qal == "720"):
+            system('youtube-dl --add-metadata -f "best[height<=720]" --recode-video mp4 -o "'+name+'1" '+url)
+        elif(qal == "1080"):
+            system('youtube-dl --add-metadata -f "best[height<=1080]" --recode-video mp4 -o "'+name+'1" '+url)
+        elif(qal == "9999"):
+            system('youtube-dl --add-metadata -f "best" --recode-video mp4 -o "'+name+'1" '+url)
+        else:
+            error = 1
+    else:
+        error = 1
+        
     ret = send_file(name+'1', as_attachment=True)
     return ret
 
